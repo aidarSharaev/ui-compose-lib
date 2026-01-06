2da1e9c0b6350f73d726b623ed2f93cf666bde80
aidar <aidar.sharaev@yandex.ru>
2026-01-07 00:49:27 +0300

    fixed ui-lib

--- ui-compose-lib/src/main/kotlin/ru/sharaev/ui_compose_lib/designsystem/DSIconButton.kt
+++ ui-compose-lib/src/main/kotlin/ru/sharaev/ui_compose_lib/designsystem/DSIconButton.kt
@@ -1,8 +1,79 @@
 package ru.sharaev.ui_compose_lib.designsystem
 
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+
+abstract class BaseDSIconModel {
+
+    abstract val tint: Color?
+
+    abstract val description: String
+
+    abstract val onClick: (() -> Unit)?
+
+    abstract val enabled: Boolean
+
+    @Composable
+    abstract fun Show(modifier: Modifier)
+}
+
+class DSVectorModelImpl(
+    val vector: ImageVector,
+    override val tint: Color? = null,
+    override val description: String = "",
+    override val enabled: Boolean = true,
+    override val onClick: (() -> Unit)? = null,
+) : BaseDSIconModel() {
+
+    @Composable
+    override fun Show(modifier: Modifier) {
+        Icon(
+            imageVector = vector,
+            contentDescription = description,
+            tint = color(),
+        )
+    }
+}
+
+class DSPainterModelImpl(
+    @DrawableRes val painter: Int,
+    override val tint: Color? = null,
+    override val description: String = "",
+    override val enabled: Boolean = true,
+    override val onClick: (() -> Unit)? = null,
+) : BaseDSIconModel() {
+
+    @Composable
+    override fun Show(modifier: Modifier) {
+        Icon(
+            painter = painterResource(id = painter),
+            contentDescription = description,
+            tint = color(),
+        )
+    }
+}
+
+@Composable
+private fun BaseDSIconModel.color(): Color = when {
+    !enabled -> {
+        MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+    }
+
+    (tint != null) -> {
+        tint ?: MaterialTheme.colorScheme.onSurface
+    }
+
+    else -> {
+        MaterialTheme.colorScheme.onSurface
+    }
+}
 
 @Composable
 fun DSIconButton(
